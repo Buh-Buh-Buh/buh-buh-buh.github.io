c3f52ebe196a6bdc25159b6a5f6bc23802dafa99
Mudit Gupta <muditg317@gmail.com>
2020-08-19 18:12:38 -0400

    add proposal component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ProjectProposalComponent } from './project-proposal/project-proposal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'proposal', component: ProjectProposalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectProposalComponent } from './project-proposal/project-proposal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectProposalComponent
   ],
   imports: [
     BrowserModule,
