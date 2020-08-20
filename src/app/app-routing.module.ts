077f77d9390edc390c7d54675302476d930516fe
Alex Kim <alexkimt@gmail.com>
2020-08-20 16:38:11 -0400

    Add components folder. Create main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ProjectProposalComponent } from './project-proposal/project-proposal.component';
+import { ProjectProposalComponent } from './components/project-proposal/project-proposal.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: MainComponent },
   { path: 'proposal', component: ProjectProposalComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectProposalComponent } from './project-proposal/project-proposal.component';
+import { ProjectProposalComponent } from './components/project-proposal/project-proposal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectProposalComponent
+    ProjectProposalComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
