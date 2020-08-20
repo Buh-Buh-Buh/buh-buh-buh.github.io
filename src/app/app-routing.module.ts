f8e4c38d0ea31a7e438f7f49a3ca13bab051b4d2
Alex Kim <alexkimt@gmail.com>
2020-08-20 17:42:47 -0400

    Remove unused project-proposal component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { ProjectProposalComponent } from './components/project-proposal/project-proposal.component';
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'proposal', component: ProjectProposalComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectProposalComponent } from './components/project-proposal/project-proposal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './components/main/main.component';
 
@@ -19,7 +18,6 @@ import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectProposalComponent,
     MainComponent
   ],
   imports: [
